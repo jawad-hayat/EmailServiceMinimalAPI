05d19752096d47ff7c3f929639af6d2b18ac1224
Jawad <jawad.h@pacsquare.com>
2024-06-24 13:04:42 +0500

    swagger implemented

--- Program.cs
+++ Program.cs
@@ -25,7 +25,7 @@ builder.Services.AddTransient<IEmailService, EmailService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
